0522714cc75bb0263477ca447d7cd299c40a076b
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-09 20:13:50 +0600

    Add project files.

---
+++ SyncLesson/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Threading;
+
+namespace SyncLesson
+{
+    public class Account : ContextBoundObject
+    {
+        private object syncObject = new object();
+        public int Sum { get; set; } = 1000;
+        public void ProcessMoney(object sum)
+        {
+            lock (syncObject)
+            {
+                Sum += (int)sum;
+                Console.WriteLine(Sum);
+            }
+        }
+    }
+}
---
+++ SyncLesson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace SyncLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+            for(int i = 0; i < 100; i++)
+            {
+                if(i % 2 == 0) 
+                    ThreadPool.QueueUserWorkItem(account.ProcessMoney, 100);
+                else
+                    ThreadPool.QueueUserWorkItem(account.ProcessMoney, -100);
+                Thread.Sleep(5);
+            }
+            Console.ReadLine();
+        }
+    }
+}
